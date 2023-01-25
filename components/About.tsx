cd33462d6daaf7c0215eb9d5ccc790b170569eb5
EB <44556455+EBhub@users.noreply.github.com>
2023-01-25 15:54:10 +0100

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -1,38 +1,21 @@
-import Link from "next/link";
 import React from "react";
 
 function AboutMe() {
     return (
-        <div className="max-w-[1080px] pt-14 text-lg">
+        <div className="max-w-[1080px] pt-14 text-xl">
             <p>
-                As a highly curious individual with a background in computer science and a passion
-                for building and creating, I am well-equipped to tackle challenging projects and
-                deliver exceptional results. My experience in the field of cryptocurrencies,
-                including 5+ years of trading and a role as a research lead for a crypto investment
-                fund, has honed my analytical skills and given me a deep understanding of the
-                technology and its applications.
-                <br />
-                <br />I am a strong proponent of free and open-source software (FOSS) and am always
-                eager to learn new skills and technologies. My successful completion of{" "}
-                <Link href="https://ethernaut.openzeppelin.com/">
-                    <span className="underline">Ethernaut</span>
-                </Link>{" "}
-                and{" "}
-                <Link href="https://www.damnvulnerabledefi.xyz/">
-                    <span className="underline">Damn Vulnerable DeFi</span>
-                </Link>{" "}
-                CTFs demonstrates my ability to independently learn new technologies, solve complex
-                puzzles and think critically.
-                <br />
-                <br />
-                With a desire to use my engineering and development skills to participate in
-                building innovative products, I am seeking opportunities to work with smart and
-                motivated individuals on challenging projects. I am confident in my ability to
-                deliver exceptional results and am excited to see what new challenges and
-                opportunities the future holds.
-                <br />
+                Hello there! I'm Elmedin, a software engineer and proponent for free open-source
+                software (FOSS).
+                <br /> <br />
+                I'm currently looking for a new role where I can work on challanging problems with
+                smart people. I'm passionate about technology, finance, and art. I also quite enjoy
+                staring at my screen while coding on a constant state of flow.
+                <br /> <br />
+                Below are some of the technologies and tools I've worked with and what I would like
+                to learn more about. If you'd like to chat, feel free to send me an e-mail.
+                <br /> <br />
             </p>
-            <p className="pt-10 text-base">
+            <p className="pt-10 text-lg">
                 Technologies I am familiar with and enjoy using: <br />
                 <ul className="list-disc list-inside">
                     <li>{"{Java,Type}"}Script </li>
