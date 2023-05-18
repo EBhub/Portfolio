a2aa5d0fa30e05365b142be1d4cdae69b7c4fac3
EB <44556455+EBhub@users.noreply.github.com>
2023-05-18 23:24:30 +0200

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -7,31 +7,29 @@ function AboutMe() {
                 Hello there! I'm Elmedin, a software engineer and proponent for free open-source
                 software (FOSS).
                 <br /> <br />
-                I'm currently looking for a new role where I can work on challenging problems with
-                smart people. I'm passionate about technology, finance, and art. I also quite enjoy
-                staring at my screen while coding in a constant state of flow.
+                I'm passionate about technology, finance, and security. I also quite enjoy
+                staring at my screen.
             </p>
             <p className="pt-10 text-lg">
                 Technologies I am familiar with and enjoy using: <br />
                 <ul className="list-disc list-inside">
-                    <li>{"{Java,Type}"}Script </li>
                     <li>Solidity</li>
+                    <li>Python</li>
+                    <li>{"{Java,Type}"}Script </li>
                     <li>Next.js</li>
                     <li>React</li>
                     <li>Node.js</li>
-                    <li>Python</li>
                     <li>Java</li>
                     <li>SQL</li>
                 </ul>
                 <br />
                 Technologies and topics I would like to learn more about: <br />
                 <ul className="list-disc list-inside">
+                    <li>Smart contract security</li>
+                    <li>Machine learning</li>
                     <li>Rust</li>
                     <li>Go</li>
-                    <li>GraphQL</li>
                     <li>C++</li>
-                    <li>Machine learning</li>
-                    <li>Smart contract security</li>
                 </ul>
             </p>
             <p>
